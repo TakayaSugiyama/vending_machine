7ad8b6b52b90f84f2e70d27bc2891453afcd5616
takayasugiyama <s.takaya1027@gmail.com>
2019-09-25 11:41:43 +0900

    ステップ4まで

--- sample.rb
+++ sample.rb
@@ -1,8 +1,7 @@
 require "./drink"
-require "./money"
-
 
 class VendingMachine
+   attr_reader :sales
     # ステップ０　お金の投入と払い戻しの例コード
     # ステップ１　扱えないお金の例コード
     # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
@@ -12,6 +11,7 @@ class VendingMachine
     def initialize
       # 最初の自動販売機に入っている金額は0円
       @slot_money = 0
+      @sales = 0
       @drinks = []
       5.times { @drinks << Drink.new("コーラ", 120)}
     end
@@ -54,22 +54,134 @@ class VendingMachine
       count
     end
     
-    #格納されているジュースの情報（値段と名前と個数）を取得できる。
+    #格納されているジュースの情報（値段と名前）を取得できる。
     def data_juice
       added_names = []
       results = []
-       @drinks.map {|drink| drink.data}.each do |k,v|
-        names << k
-         if names.count(k) == 1
-            results <<  k
+       @drinks.map {|drink| drink.data}.each do |data|
+        added_names << data
+         if added_names.count(data) == 1
+            results <<  data
          end
        end 
        results
     end
+
+    # 格納されているジュースの情報（値段と名前と個数）を表示できる。
+    def show_drinks
+      self.data_juice.each do |data|
+        data.each do |name,price|
+           puts  "#{price}円の#{name}が#{self.count(name.to_s)}本"
+        end
+      end
+    end
+
+    #コーラを買えるかどうかを返す
+    def buy_coke?
+      if @slot_money >= 120  && self.count("コーラ") > 0
+        "買えます"
+      else 
+        "買えません"
+      end
+    end
+
+    #ドリンクを追加する 
+   def add(name,price,count)
+       count.times {@drinks << Drink.new(name,price) }
+   end
+
+   #値段を返す
+   def price(buy_drink_name)
+    drink_price = 0
+    self.data_juice.each do |data|
+      data.each do |name,price|
+         drink_price += price.to_i  if buy_drink_name == name.to_s
+      end
+    end
+     drink_price
+   end
+
+   #ジュース値段以上の投入金額が投入されている条件下で購入操作を行うと、ジュースの在庫を減らし、売り上げ金額を増やす。
+   def buy(name)
+      if self.count(name) > 0 && @slot_money >= self.price(name)
+        count = 0
+        @sales +=  self.price(name)
+        @slot_money -=  self.price(name)
+        @drinks.each do |drink|
+          if drink.name == name 
+             @drinks.delete(drink) if count == 0
+             count += 1
+          end
+        end
+        "購入しました"
+      else 
+          "買えませんでした。"
+      end
+   end
+
+   #投入金額、在庫の点で購入可能なドリンクのリストを表示する。
+   def drink_lists
+      results = []
+      self.data_juice.each do |data|
+        data.each do |name,price|
+           results << name.to_s  if @slot_money >= price.to_i
+        end
+      end
+      results
+   end
 
 end
 
 
   # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
  vm = VendingMachine.new
- p vm.data_juice
+
+ puts "初期状態で、コーラ（値段:120円、名前”コーラ”）を5本格納している。"
+ vm.show_drinks
+
+ puts "--------------------------------------------------"
+ puts "リラックマ茶を10本追加する"
+ vm.add("リラックマ茶", 140,10)
+ vm.show_drinks
+
+ puts "--------------------------------------------------"
+ puts "500円を投入する"
+ vm.slot_money(500)
+ puts "残高: #{vm.current_slot_money}円"
+
+ puts "投入金額、在庫の点で、コーラが購入できるかどうかを取得できる。"
+ puts  vm.buy_coke?
+
+ puts "--------------------------------------------------"
+
+ puts "コーラを1本購入する"
+ vm.buy("コーラ")
+
+ puts "リラックマ茶を1本購入する"
+ vm.buy("リラックマ茶")
+
+ puts "リラックマ茶を1本購入する"
+ vm.buy("リラックマ茶")
+
+ puts "売上を表示する"
+ puts vm.sales.to_s + "円"
+ puts "--------------------------------------------------"
+
+puts "在庫を確認"
+vm.show_drinks
+
+puts "--------------------------------------------------"
+puts "在庫にレッドブル（値段:200円、名前”レッドブル”）5本を追加する。"
+vm.add("レッドブル", 200, 5)
+puts "在庫に水（値段:100円、名前”水”）5本を追加する。"
+vm.add("水", 100, 5)
+
+puts "--------------------------------------------------"
+puts "投入金額、在庫の点で購入可能なドリンクのリストを取得できる。"
+puts "残高: #{vm.current_slot_money}円"
+p vm.drink_lists
+
+
+
+
+ 

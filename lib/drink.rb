4a229af1dd7d7d04aa20e1d2a8a2aafbf002e7e1
takayasugiyama <s.takaya1027@gmail.com>
2019-11-15 20:14:56 +0900

    Drinkクラスとテストを変更

--- lib/drink.rb
+++ lib/drink.rb
@@ -1,20 +1,15 @@
 class Drink 
   attr_reader :name, :price
+  DRINKLIST = {"コーラ" => 120, "水" => 100, "レッドブル" => 200 }
+
   def initialize(name)
     @name = name 
-    if name == 'コーラ'
-      @price = 120
-    elsif name == 'レッドブル'
-      @price = 200
-    elsif name == '水'
-      @price = 100
-    else
-      puts "販売できません"
-    end
+    @price = DRINKLIST[name]
   end
   
-  #ハッシュ化
-  def drink_to_hash
-   {name: @name, price: @price}
+  #値段を返す
+  def self.how_match?(name)
+    DRINKLIST.keys.include?(name) ? DRINKLIST[name] : "在庫がありません"
   end
+
 end
--- test/drink_test.rb
+++ test/drink_test.rb
@@ -16,9 +16,9 @@ class DrinkTest < Minitest::Test
     assert @redbule
   end
 
-  def test_drink_to_hash
-    assert_equal @water.drink_to_hash , {name: "水", price: 100}
-    assert_equal @coke.drink_to_hash , {name: "コーラ", price: 120}
-    assert_equal @redbule.drink_to_hash , {name: "レッドブル", price: 200}
+  def test_how_match?
+    assert_equal Drink.how_match?("コーラ"), 120
+    assert_equal Drink.how_match?("水"), 100 
+    assert_equal Drink.how_match?("レッドブル"), 200
   end
 end
